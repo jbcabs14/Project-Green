d8e9165082aa3896ebcc09dda72e6082068b06c7
John Benedict Cabintoy <jbenedictcabintoy14@gmail.com>
2025-04-05 18:23:44 +0800

    delete unnecessary files

--- lib/common/classes/navigation_destination_item.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class NavigationDestinationItem {
-  NavigationDestinationItem({this.name = '', required this.widget, required this.iconData});
-
-  final String name;
-  final Widget widget;
-  final IconData iconData;
-}
--- lib/features/main/controllers/registrants/refresh_indicator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:liquid_pull_refresh/liquid_pull_refresh.dart';
-
-class RegistrantsRefreshIndicatorController extends GetxController {
-  RegistrantsRefreshIndicatorController get instance => Get.find();
-
-  final key = GlobalKey<LiquidPullRefreshState>();
-}
